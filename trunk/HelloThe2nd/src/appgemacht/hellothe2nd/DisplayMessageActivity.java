edcaf06198088fb9296e0feada904bd455a8da03
dikndd@gmail.com <dikndd@gmail.com@847bec07-d499-c65b-acea-90ce50e8649f>
2013-09-20 21:43:27 +0000

    - add the getIntent() stuff to show the entered text in the DisplayMessageActivity

--- trunk/HelloThe2nd/src/appgemacht/hellothe2nd/DisplayMessageActivity.java
+++ trunk/HelloThe2nd/src/appgemacht/hellothe2nd/DisplayMessageActivity.java
@@ -1,15 +1,40 @@
 package appgemacht.hellothe2nd;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_display_message);
+		
+		// Get the message from the intent
+		// (Every Activity is invoked by an Intent and the data contained with
+		//  it can be retrieved via getIntent() )
+	    Intent intent = getIntent();
+	    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+		
+	    // Create the text view
+	    TextView textView = new TextView(this);
+	    textView.setTextSize(40);
+	    textView.setText(message);
+
+		// Set the text view as the activity layout
+		setContentView(textView);//R.layout.activity_display_message);
+		
+		// Make sure we're running on Honeycomb or higher to use ActionBar APIs
+		//if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+		//	if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.FROYO) {
+		//		// Show the Up button in the action bar.
+		//		getActionBar().setDisplayHomeAsUpEnabled(true);
+		//	}
+		//}
 	}
 
 	@Override
@@ -18,5 +43,16 @@ public class DisplayMessageActivity extends Activity {
 		getMenuInflater().inflate(R.menu.display_message, menu);
 		return true;
 	}
+
+	// don't know yet, what to do with this ... let it in just ...
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
