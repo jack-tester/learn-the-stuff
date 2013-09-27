3d554f99427d7098bc17668b66aaf5065bc8ee3c
dikndd@gmail.com <dikndd@gmail.com@847bec07-d499-c65b-acea-90ce50e8649f>
2013-09-27 00:37:56 +0000

    added a search button with fancy icon in the ActionBar

--- trunk/HelloThe2nd/src/appgemacht/hellothe2nd/MainActivity.java
+++ trunk/HelloThe2nd/src/appgemacht/hellothe2nd/MainActivity.java
@@ -1,10 +1,10 @@
 package appgemacht.hellothe2nd;
 
-import android.support.v7.app.ActionBarActivity;
-//import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.EditText;
 
@@ -25,8 +25,9 @@ public class MainActivity extends ActionBarActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main_activity_actions, menu);
+	    return super.onCreateOptionsMenu(menu);
 	}
 
 	/** Called when the user clicks the Send button
