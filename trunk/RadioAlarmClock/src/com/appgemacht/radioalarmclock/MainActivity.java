9f5bb2016b6c3b198f346028afee08617d2f4a74
dikndd <dikndd@847bec07-d499-c65b-acea-90ce50e8649f>
2014-02-05 23:59:16 +0000

    change ContentView in MainActivity from 'activity_main' to 'clock'

--- trunk/RadioAlarmClock/src/com/appgemacht/radioalarmclock/MainActivity.java
+++ trunk/RadioAlarmClock/src/com/appgemacht/radioalarmclock/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends Activity implements InternetRadioListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.clock);//activity_main);
 	}
 
 	@Override
