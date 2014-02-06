6e09be603baf132129397dff96fda6e906a50d78
dikndd <dikndd@847bec07-d499-c65b-acea-90ce50e8649f>
2014-02-06 23:48:03 +0000

    add further files and functions

--- trunk/RadioAlarmClock/src/com/appgemacht/radioalarmclock/MainActivity.java
+++ trunk/RadioAlarmClock/src/com/appgemacht/radioalarmclock/MainActivity.java
@@ -1,21 +1,73 @@
 package com.appgemacht.radioalarmclock;
 
+import java.util.Timer;
+
 import com.appgemacht.radioalarmclock.InternetRadio.InternetRadioListener;
 
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.app.Activity;
+import android.content.Context;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.appgemacht.radioalarmclock.InternetRadio.InternetRadioListener;
+import com.appgemacht.radioalarmclock.Alarms;
+import com.appgemacht.radioalarmclock.R;
+import com.appgemacht.radioalarmclock.InternetRadio;
 
 public class MainActivity extends Activity implements InternetRadioListener {
 
+	
+	public static final String ALARM_UP = "ALARM_UP"; // vom AlarmReceiver:
+	                                                  // Alarm geht los
+	
+	private TextView clockView;
+	private Button stopAlarmButton;
+	private MenuItem alarmMenu;
+
+	private Timer timer; // zum Update der Uhrzeit
+	private static PowerManager.WakeLock wakeLock;
+
+	private Alarms alarms;
+	private InternetRadio radio = new InternetRadio();
+	private int originalVolume;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.clock);//activity_main);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN); // kein Statusbar ganz oben
+		alarms = new Alarms(this);
+		clockView = (TextView) findViewById(R.id.clockTextView);
+		stopAlarmButton = (Button) findViewById(R.id.stopAlarmButton);
+		stopAlarmButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				stopAlarm();
+			}
+		});
+
+		radio.internetRadioListener = this;		
+	}
+
+	private void stopAlarm() {
+		radio.stop();
+		if (wakeLock != null && wakeLock.isHeld()) {
+			wakeLock.release();
+		}
+		wakeLock = null;
+		getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); // Bildschirm soll nicht mehr anbleiben
+		stopAlarmButton.setVisibility(View.GONE);
 	}
 
 	@Override
@@ -42,4 +94,16 @@ public class MainActivity extends Activity implements InternetRadioListener {
 		Log.v("Alarm", "Radio prepare finished");
 	}
 
+
+	
+	@SuppressWarnings("deprecation")
+	public static void accquireWakeLock(Context context) {
+		PowerManager powermgr = (PowerManager) context
+				.getSystemService(Context.POWER_SERVICE);
+//		wakeLock = powermgr.newWakeLock(PowerManager.FULL_WAKE_LOCK
+//				| PowerManager.ACQUIRE_CAUSES_WAKEUP,
+//				"RadioAlarmClock");
+//		wakeLock.acquire(3600 * 1000);
+	}
+	
 }
