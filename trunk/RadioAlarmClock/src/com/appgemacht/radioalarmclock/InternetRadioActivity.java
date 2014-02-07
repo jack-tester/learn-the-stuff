f712d5824d4008ef31de221abea26a76b4d28d16
dikndd <dikndd@847bec07-d499-c65b-acea-90ce50e8649f>
2014-02-07 23:16:43 +0000

    o.k. - man kann nichts damit anfangen ... außer: die aktuelle Uhrzeit SUPERGROSS angucken

---
+++ trunk/RadioAlarmClock/src/com/appgemacht/radioalarmclock/InternetRadioActivity.java
@@ -0,0 +1,236 @@
+package com.appgemacht.radioalarmclock;
+
+import java.io.*;
+import java.net.*;
+import java.util.regex.Matcher;
+
+import android.app.Activity;
+import android.content.*;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.os.*;
+import android.text.*;
+import android.util.*;
+import android.view.*;
+import android.view.View.OnClickListener;
+import android.widget.*;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+
+import com.appgemacht.radioalarmclock.InternetRadio.InternetRadioListener;
+
+/**
+ * Activity zum Setzen der Radio-Parameter.
+ * Diese Activity wird als Popup angezeigt (siehe Definition im AndroidManifest) 
+ *
+ */
+public class InternetRadioActivity extends Activity implements InternetRadioListener {
+	private final InternetRadio radio=new InternetRadio();
+	private AudioManager audioManager;
+	private int originalVolume; // Wiederherstellen der Original-Lautstärke beim Verlassen der Activity
+	private TextView editStreamURL;
+	private SeekBar seekVolume;
+	private TextView statusView;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.setradio);
+		setTitle("Radio");
+		
+		editStreamURL = (TextView) findViewById(R.id.editStreamURL);
+		editStreamURL.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+			}			
+			@Override
+			public void afterTextChanged(Editable s) {
+				radio.storeStreamURL(InternetRadioActivity.this,editStreamURL.getText().toString());
+				startRadio();
+			}
+		});
+		statusView = (TextView) findViewById(R.id.statusView);
+		
+		seekVolume = (SeekBar)findViewById(R.id.seekVolume);
+		audioManager=(AudioManager) getSystemService(Context.AUDIO_SERVICE);
+		seekVolume.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+		seekVolume.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+			}			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+			}
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+				if(fromUser) {
+					audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+					radio.storeVolume(InternetRadioActivity.this,  progress);
+				}
+			}
+		});
+		
+		((ImageButton)findViewById(R.id.lookupStations)).setOnClickListener(new OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				lookupStations();				
+			}
+		});
+		
+
+		radio.internetRadioListener=this;
+	}
+
+	protected void lookupStations() {
+		String url = "http://www.listenlive.eu/germany.html";
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse(url));
+		startActivity(i);		
+		// Hinweis was zu tun ist
+		Toast.makeText(this, R.string.ClickStreamLink, Toast.LENGTH_LONG).show();
+	}
+
+	private void startRadio() {
+		if (radio.loadStreamURL(this).length() != 0) {
+			statusView.setText(R.string.LoadingStation);			
+			radio.start(this, false);
+		}
+		else
+			statusView.setText(R.string.NoRadioStream);
+
+	}
+	@Override
+	public void onInternetRadioPrepareFinished() {
+		statusView.setText("");		
+	}
+	@Override
+	public void onInternetRadioError(String error) {
+		statusView.setText(error);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		originalVolume=audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+		editStreamURL.setText(radio.loadStreamURL(this));
+		seekVolume.setProgress(radio.loadVolume(this));
+
+		Uri uri = getIntent().getData();
+		if (uri != null) {
+			// network activity not allowed on UI thread
+			AsyncTask<Uri, Void, Void> asyncTask = new AsyncTask<Uri, Void, Void>() {
+				@Override
+				protected Void doInBackground(Uri... uri) {
+					Log.v("Radio","Open URI "+uri[0]);
+					final String streamURL = openURI(uri[0]);
+					Log.v("Radio","Stream URL "+streamURL);
+					if (streamURL != null) {
+						// back to UI thread for updating text
+						runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								editStreamURL.setText(streamURL);
+								radio.storeStreamURL(InternetRadioActivity.this, streamURL);
+								startRadio();
+							}
+						});
+					}
+					return null;
+				}
+			};
+			asyncTask.execute(uri);
+		}
+		getIntent().setData(null);  // nicht mehrfach öffnen
+		startRadio();
+	}
+	
+	
+
+	private String openURI(Uri uri) {
+		// handle local and remote uris
+		String data;
+		if (uri.getScheme() != null && uri.getScheme().startsWith("http"))
+			data = downloadRemoteURI(uri); // remote file, z.B. Browser-Link
+		else
+			data = openLocalURI(uri); // local file, z.B. Mail-Anhang
+		if (data != null) {
+			Matcher matcher = Patterns.WEB_URL.matcher(data);
+			if (matcher.find()) {
+				String streamURL = matcher.group();
+				if(!streamURL.startsWith("http")) // manchmal lässt der Matcher das Schema weg 
+					streamURL="http://"+streamURL;
+				Log.v("Radio", "Stream URL: " + streamURL);
+				return streamURL;
+			}
+		}
+		return null;
+	}
+
+	private String openLocalURI(Uri uri) {
+		InputStream is = null;
+		try {
+			is = getContentResolver().openInputStream(uri);
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is,
+					"UTF8"), 4096);
+			String line;
+			StringBuilder sb = new StringBuilder();
+			while ((line = rd.readLine()) != null) {
+				sb.append(line);
+			}
+			rd.close();
+			return sb.toString();
+		} catch (Exception e) {
+			Log.e("uri", "No data from " + uri);
+			return null;
+		} finally {
+			if (is != null)
+				try {
+					is.close();
+				} catch (IOException e) {
+					Log.v("uri", e.getLocalizedMessage());
+				}
+		}
+	}
+
+	private String downloadRemoteURI(Uri uri) {
+		InputStream is = null;
+		try {
+			URL url = new URL(uri.toString());
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setConnectTimeout(10 * 1000);
+			is = conn.getInputStream();
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is,
+					"UTF8"), 4096);
+			String line;
+			StringBuilder sb = new StringBuilder();
+			while ((line = rd.readLine()) != null) {
+				sb.append(line+"\n");
+			}
+			rd.close();
+			return sb.toString();
+
+		} catch (IOException e) {
+			Log.e("Radio", "Error downloading " + uri, e);
+			return null;
+		} finally {
+			if (is != null)
+				try {
+					is.close();
+				} catch (IOException e) {
+					Log.v("uri", e.getLocalizedMessage());
+				}
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		radio.stop();
+		audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, originalVolume, 0);
+		super.onPause();
+	}
+}
--- trunk/RadioAlarmClock/src/com/appgemacht/radioalarmclock/MainActivity.java
+++ trunk/RadioAlarmClock/src/com/appgemacht/radioalarmclock/MainActivity.java
@@ -1,19 +1,28 @@
 package com.appgemacht.radioalarmclock;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Timer;
+import java.util.TimerTask;
 
 import com.appgemacht.radioalarmclock.InternetRadio.InternetRadioListener;
 
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.View.OnClickListener;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,6 +31,10 @@ import com.appgemacht.radioalarmclock.InternetRadio.InternetRadioListener;
 import com.appgemacht.radioalarmclock.Alarms;
 import com.appgemacht.radioalarmclock.R;
 import com.appgemacht.radioalarmclock.InternetRadio;
+import com.appgemacht.radioalarmclock.Alarm;
+import com.appgemacht.radioalarmclock.MainActivity;
+//import com.appgemacht.radioalarmclock.AlarmsActivity;
+import com.appgemacht.radioalarmclock.InternetRadioActivity;
 
 public class MainActivity extends Activity implements InternetRadioListener {
 
@@ -60,6 +73,116 @@ public class MainActivity extends Activity implements InternetRadioListener {
 		radio.internetRadioListener = this;		
 	}
 
+	// Hilfsfunktion um Text in einem TextView so groß wie möglich darzustellen
+	// Im Layout ggf. android:singleLine="true" 
+	// und android:includeFontPadding="false" setzen
+	private void maximize(TextView textView) {
+		// gehe von maximaler Höhe aus und verkleinere bis nicht mehr abgeschnitten wird
+		// Achtung: textView.getTextSize() gibt Pixel zurück
+		// während textView.setTextSize() dips erwartet
+		final CharSequence text = textView.getText();
+		final float scale = getResources().getDisplayMetrics().density;
+		final float width=textView.getWidth()-textView.getPaddingLeft()-textView.getPaddingRight();
+		final float height=textView.getHeight()-textView.getCompoundPaddingTop()-textView.getCompoundPaddingBottom();
+		for (float textSize = height/scale; textSize > 10; textSize *= 0.95) {
+			textView.setTextSize(textSize); // dip
+			if (text.equals(TextUtils.ellipsize(text, textView.getPaint(),
+					width, TextUtils.TruncateAt.END))) // Text passt 
+				break;
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		AudioManager audioManager=(AudioManager) getSystemService(Context.AUDIO_SERVICE);
+		originalVolume=audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+		// Timer zur Aktualisierung der Uhrzeit
+		timer = new Timer(getString(R.string.app_name));
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				// TimerTask läuft in einem Background-Thread, 
+				// UI-Änderungen (z.B. Text setzen)
+				// dürfen aber nur vom UI-Thread aus gemacht werden
+				runOnUiThread(new Runnable() {
+					public void run() {
+						updateClock();
+					}
+				});
+			}
+		}, 0, 1000); // wiederhole alle 1000 ms
+		
+		// Warnung falls keine Radio-URL vorhanden
+//		if(radio.loadStreamURL(this).length()==0)
+//			Toast.makeText(this, R.string.NoRadioStream, Toast.LENGTH_LONG).show();
+
+		// Intent wird vom BroadcastReceiver gesetzt, wenn ein Alarm fällig ist
+		if (getIntent().getBooleanExtra(ALARM_UP, false)) {
+			getIntent().removeExtra(ALARM_UP); // vermeide Mehrfach-Alarm
+			onAlarmUp();
+		}
+	}
+	
+	@Override
+	protected void onPause() {
+		stopAlarm();
+		timer.cancel();		
+		AudioManager audioManager=(AudioManager) getSystemService(Context.AUDIO_SERVICE);
+		audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, originalVolume, 0);
+		super.onPause();
+	}
+	
+	private void onAlarmUp() {
+		Log.v("Alarm","onAlarmUp");
+		accquireWakeLock(this);
+		getWindow().addFlags(
+				WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED						
+						| WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+						| WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		flashClock();
+		// starte Radio mit geringer Verzögerung, 
+		// damit das System aufwachen und eine Internet-Verbindung herstellen kann 
+		new Timer().schedule(new TimerTask() {
+			@Override
+			public void run() {
+				alarms.scheduleNextAlarm(MainActivity.this);
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						radio.start(MainActivity.this, true);
+						stopAlarmButton.setVisibility(View.VISIBLE);
+					}
+				});
+			}
+		}, 1000);
+		// Notfall-Fallback, falls Radio nicht abgespielt werden kann
+		// (z.B. keine Internet-Verbindung)
+		new Timer().schedule(new TimerTask() {
+			@Override
+			public void run() {
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						if(!radio.wasStarted()) {
+							radio.playDefaultAlarm(MainActivity.this);
+							stopAlarmButton.setVisibility(View.VISIBLE);
+						}
+					}
+				});
+			}
+		}, 20000);
+	}
+
+	// kleine Animation zum Blinken der Uhrzeit
+	private void flashClock() {
+		Animation anim = new AlphaAnimation(0.0f, 1.0f);
+		anim.setDuration(200); // in ms
+		anim.setRepeatMode(Animation.REVERSE);
+		anim.setRepeatCount(30);
+		clockView.startAnimation(anim);
+	}
+
 	private void stopAlarm() {
 		radio.stop();
 		if (wakeLock != null && wakeLock.isHeld()) {
@@ -75,6 +198,62 @@ public class MainActivity extends Activity implements InternetRadioListener {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.alarm:
+			showSetAlarm();
+			break;
+		case R.id.radio:
+			showSetRadio();
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void showSetRadio() {
+		startActivity(new Intent(this, InternetRadioActivity.class));
+	}
+
+	private void showSetAlarm() {
+//		startActivityForResult(new Intent(this, AlarmsActivity.class), 0);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		alarms = new Alarms(this); // re-load alarms
+		alarms.scheduleNextAlarm(this);
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	private void updateClock() {
+		Calendar c = Calendar.getInstance();
+		DateFormat sdf = SimpleDateFormat
+				.getTimeInstance(SimpleDateFormat.SHORT);
+		clockView.setText(sdf.format(c.getTime()));
+		maximize(clockView);
+		// update alarm entry
+		if (alarmMenu != null) {
+			Alarm nextAlarm = alarms.nextAlarm();
+			if (nextAlarm != null && alarms.isActive(this))
+				alarmMenu.setTitle("Alarm: " + nextAlarm.toString());
+			else
+				alarmMenu.setTitle("Alarm: Aus");
+		}
+		if (stopAlarmButton.getVisibility() == View.VISIBLE)
+			maximize(stopAlarmButton);
+	}
+
+	@SuppressWarnings("deprecation")
+	public static void accquireWakeLock(Context context) {
+		PowerManager powermgr = (PowerManager) context
+				.getSystemService(Context.POWER_SERVICE);
+//		wakeLock = powermgr.newWakeLock(PowerManager.FULL_WAKE_LOCK
+//				| PowerManager.ACQUIRE_CAUSES_WAKEUP,
+//				"RadioAlarmClock");
+//		wakeLock.acquire(3600 * 1000);
 	}
 
 	/*
@@ -94,16 +273,4 @@ public class MainActivity extends Activity implements InternetRadioListener {
 		Log.v("Alarm", "Radio prepare finished");
 	}
 
-
-	
-	@SuppressWarnings("deprecation")
-	public static void accquireWakeLock(Context context) {
-		PowerManager powermgr = (PowerManager) context
-				.getSystemService(Context.POWER_SERVICE);
-//		wakeLock = powermgr.newWakeLock(PowerManager.FULL_WAKE_LOCK
-//				| PowerManager.ACQUIRE_CAUSES_WAKEUP,
-//				"RadioAlarmClock");
-//		wakeLock.acquire(3600 * 1000);
-	}
-	
 }
