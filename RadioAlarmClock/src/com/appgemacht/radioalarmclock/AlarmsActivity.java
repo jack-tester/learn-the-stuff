a3b2270a01fea6c61e63b8bdc5beba0088af8274
dikndd <dikndd@847bec07-d499-c65b-acea-90ce50e8649f>
2014-02-08 23:49:44 +0000

    progress made in settings menu; however, the setalarms menu item still crashes down the app

---
+++ RadioAlarmClock/src/com/appgemacht/radioalarmclock/AlarmsActivity.java
@@ -0,0 +1,154 @@
+package com.appgemacht.radioalarmclock;
+
+import java.util.*;
+
+// http://actionbarsherlock.com/usage.html
+//import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+//import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+
+//import android.app.DialogFragment;
+
+//import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
+import android.widget.AdapterView.OnItemClickListener;
+//import android.widget.CompoundButton.OnCheckedChangeListener;
+
+public class AlarmsActivity extends SherlockFragmentActivity {
+	public Alarms alarms;
+	private ListView listView;
+	private AlarmArrayAdapter alarmArrayAdapter;
+	private final List<List<Alarm>> alarmGroups=new ArrayList<List<Alarm>>();
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    alarms=new Alarms(this);
+	    setContentView(R.layout.setalarms);
+	    setTitle("Alarms");
+	    getSupportActionBar().setHomeButtonEnabled(true); // tipp auf das App-Icon oben links führt zurück (android.R.id.home optione menu)
+	    
+//	    // Switch in der ActionBar
+//	    getSupportActionBar().setDisplayShowCustomEnabled(true);
+//	    Switch alarmsActive=new Switch(this); // ab 4.0, CheckBox für ältere Releases
+//	    alarmsActive.setChecked(alarms.isActive(this));
+//	    alarmsActive.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+//			
+//			@Override
+//			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//				alarms.setActive(AlarmsActivity.this, isChecked);				
+//			}
+//		});
+//	    getSupportActionBar().setCustomView(alarmsActive);
+//	    alarmsActive.setChecked(true); // beim Öffnen der Activity soll der Switch per default on sein
+	    
+	    // Liste mit Alarm-Gruppen
+	    listView=(ListView)findViewById(android.R.id.list);
+	    alarmArrayAdapter = new AlarmArrayAdapter(this,alarmGroups);
+		listView.setAdapter(alarmArrayAdapter);
+	    listView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> adapter, View view,
+					int position, long id) {
+				selectAlarmItem(position);
+
+			}
+	    }
+		);
+
+	}
+
+	private void selectAlarmItem(int position) {
+		List<Alarm> alarmGroup = alarmGroups.get(position);
+		listView.setItemChecked(position, true);
+
+		SetAlarmFragment alarmFragment = new SetAlarmFragment();
+		alarmFragment.load(alarmGroup);
+		alarmFragment.show(getSupportFragmentManager(), null);
+	}
+
+	@Override
+	protected void onResume() {		
+		super.onResume();
+		refresh();
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home: // Tipp auf Icon oben links in actionbar
+			finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public void refresh()
+	{
+		// Alarme haben sich geändert, frische Liste auf 
+		alarmGroups.clear();
+		alarmGroups.addAll(alarms.alarmGroups());
+		if(alarmGroups.size()!=7)
+			alarmGroups.add(new ArrayList<Alarm>()); // "Add" button
+		
+		alarmArrayAdapter.notifyDataSetChanged();		
+	}
+	
+	
+	@Override
+	protected void onPause() {
+		alarms.storeAlarms(this);
+		super.onPause();
+	}
+
+	// ListAdapter, der die Darstellung der Alarme in der Liste festlegt
+	private class AlarmArrayAdapter extends ArrayAdapter<List<Alarm>> {
+		private List<List<Alarm>> alarmGroups;
+
+		public AlarmArrayAdapter(Context context, List<List<Alarm>> alarmGroups) {
+			super(context, android.R.layout.simple_list_item_2,
+					android.R.id.text1,alarmGroups);
+			this.alarmGroups = alarmGroups;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			List<Alarm> alarmGroup=alarmGroups.get(position);
+			View row = super.getView(position, convertView, parent);
+				
+			
+			TextView text1 = (TextView) row.findViewById(android.R.id.text1);
+			TextView text2 = (TextView) row.findViewById(android.R.id.text2);
+			if(alarmGroup.size()==0) {
+				text1.setText(R.string.Add);
+				text2.setText("");
+				return row;
+			}
+			// Wochentage
+			StringBuffer weekdays=new StringBuffer();
+			for(Alarm alarm:alarmGroup)
+			{
+				if(weekdays.length()!=0)
+					weekdays.append(", ");
+				weekdays.append(Alarm.weekdayAsShortString(alarm.dayOfWeek));
+			}
+			text1.setText(weekdays);
+
+			// Uhrzeit
+			Alarm alarm=alarmGroup.get(0);
+			text2.setText(Alarm.timeAsString(alarm.hours, alarm.minutes));
+			
+			if(listView.isItemChecked(position))
+				row.setBackgroundColor(Color.argb(0x80,0xA0,0xA0,0xA0)); // mark active cell
+			else
+				row.setBackgroundColor(Color.TRANSPARENT);
+			return row;
+		}
+	}
+}
---
+++ RadioAlarmClock/src/com/appgemacht/radioalarmclock/SetAlarmFragment.java
@@ -0,0 +1,124 @@
+package com.appgemacht.radioalarmclock;
+
+import java.util.List;
+
+import com.actionbarsherlock.app.*;
+
+//import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.*;
+import android.widget.*;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.TimePicker.OnTimeChangedListener;
+
+public class SetAlarmFragment extends SherlockDialogFragment {
+
+	private List<Alarm> alarmsGroup; // Gruppe von Alarmen mit gleicher Weckzeit
+	private TimePicker timePicker;
+	private ViewGroup weekdayButtons;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		if(getDialog()!=null)
+			getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		final View v = inflater.inflate(R.layout.setalarm, container,
+				false);
+		
+		timePicker = (TimePicker) v.findViewById(R.id.timePicker);
+		timePicker.setIs24HourView(DateFormat.is24HourFormat(getActivity()));
+		weekdayButtons = (ViewGroup) v.findViewById(R.id.weekdayButtons);
+		
+		List<Integer> weekdays = Alarm.weekdays();
+		for(int i=0;i<weekdayButtons.getChildCount();i++)
+		{
+			ToggleButton weekdayButton=(ToggleButton)weekdayButtons.getChildAt(i);
+			weekdayButton.setTextOn(Alarm.weekdayAsShortString(weekdays.get(i)));
+			weekdayButton.setTextOff(weekdayButton.getTextOn());
+			weekdayButton.setTag(weekdays.get(i));
+		}
+		update();
+
+		return v;
+	}
+	
+	private final OnCheckedChangeListener onToggleWeekdayButton=new OnCheckedChangeListener() {
+		
+		@Override
+		public void onCheckedChanged(CompoundButton button, boolean isChecked) {
+			int weekday=(Integer)button.getTag();
+			Alarm alarm=((AlarmsActivity)getActivity()).alarms.alarmsPerDayOfWeek[weekday];
+			if(isChecked)
+			{
+				alarm.active=true;
+				alarm.hours=timePicker.getCurrentHour();
+				alarm.minutes=timePicker.getCurrentMinute();
+				alarmsGroup.add(alarm);
+			}
+			else
+			{
+				alarm.active=false;
+				alarmsGroup.remove(alarm);
+			}
+			//activity.refresh(alarmsGroup); 
+		}
+	};
+	private final OnTimeChangedListener onTimeChangedListener=new OnTimeChangedListener() {
+		@Override
+		public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+			for(Alarm alarm:alarmsGroup)
+			{
+				alarm.hours = hourOfDay;
+				alarm.minutes = minute;
+			}
+			//activity.refresh(alarmsGroup);
+		}
+	};
+	
+	public void load(List<Alarm> alarmsGroup)
+	{
+		this.alarmsGroup=alarmsGroup;
+		if(timePicker!=null)
+			update();
+	}
+	@Override
+	public void onDismiss(DialogInterface dialog) {
+		((AlarmsActivity)getActivity()).refresh();
+		super.onDismiss(dialog);
+	}
+	
+	private void update()
+	{
+		// aktive Wochentage
+		for(int i=0;i<weekdayButtons.getChildCount();i++)
+		{
+			ToggleButton button = (ToggleButton) weekdayButtons.getChildAt(i);
+			button.setOnCheckedChangeListener(null); // kein Update
+			button.setChecked(false);
+			for(Alarm alarm:alarmsGroup)
+			{
+				if(alarm.dayOfWeek==(Integer)button.getTag())
+				{
+					button.setChecked(true);
+					break;
+				}
+			}
+			button.setOnCheckedChangeListener(onToggleWeekdayButton);
+		}
+		// Uhrzeit
+		timePicker.setOnTimeChangedListener(null);
+		if (alarmsGroup.size() != 0) {
+			Alarm firstAlarm = alarmsGroup.get(0);
+			timePicker.setCurrentHour(firstAlarm.hours);
+			timePicker.setCurrentMinute(firstAlarm.minutes);
+		}
+		else {
+			timePicker.setCurrentHour(7);
+			timePicker.setCurrentMinute(0);
+		}
+		timePicker.setOnTimeChangedListener(onTimeChangedListener);
+	}	
+}
