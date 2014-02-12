a9bf240de86027dad1998deb8eb0461770eea1cc
dikndd <dikndd@847bec07-d499-c65b-acea-90ce50e8649f>
2014-02-12 23:18:19 +0000

    apply new formatting - mainly tabs are spaces now ...

--- RadioAlarmClock/src/com/appgemacht/radioalarmclock/AlarmReceiver.java
+++ RadioAlarmClock/src/com/appgemacht/radioalarmclock/AlarmReceiver.java
@@ -5,24 +5,26 @@ import android.util.Log;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		if(intent.getAction()!=null && intent.getAction().equals("android.intent.action.BOOT_COMPLETED")){
-			// Ger‰t wurde gebootet, Alarm muss neu gesetzt werden
-			Alarms alarms=new Alarms(context);
-			alarms.scheduleNextAlarm(context);
-			}
-		else
-		{
-			// ein Alarm ist f‰llig, schalte Ger‰t ein und starte Radio
-			Log.v("Alarm","Alarm up");
-			MainActivity.accquireWakeLock(context);
-			
-			Intent i=new Intent(context,MainActivity.class);
-			i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // wir starten die Activity auﬂerhalb einer anderen Activity
-			i.putExtra(MainActivity.ALARM_UP, true);
-			context.startActivity(i);
-		}
-	}
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction() != null
+                && intent.getAction().equals(
+                        "android.intent.action.BOOT_COMPLETED")) {
+            // Ger‰t wurde gebootet, Alarm muss neu gesetzt werden
+            Alarms alarms = new Alarms(context);
+            alarms.scheduleNextAlarm(context);
+        } else {
+            // ein Alarm ist f‰llig, schalte Ger‰t ein und starte Radio
+            Log.v("Alarm", "Alarm up");
+            MainActivity.accquireWakeLock(context);
+
+            Intent i = new Intent(context, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // wir starten die
+                                                       // Activity auﬂerhalb
+                                                       // einer anderen Activity
+            i.putExtra(MainActivity.ALARM_UP, true);
+            context.startActivity(i);
+        }
+    }
 
 }
