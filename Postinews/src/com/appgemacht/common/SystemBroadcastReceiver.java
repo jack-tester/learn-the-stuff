3e542935f717d3a85c1ae17a5f4dab98c56fd0d3
dikndd <dikndd@847bec07-d499-c65b-acea-90ce50e8649f>
2014-07-11 22:05:40 +0000

    take the SystemBroadcastReceiver class out of the manifest ... the file is kept - see svn 119 for reference

--- Postinews/src/com/appgemacht/common/SystemBroadcastReceiver.java
+++ Postinews/src/com/appgemacht/common/SystemBroadcastReceiver.java
@@ -1,14 +1,14 @@
-package com.appgemacht.common;
-
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.widget.Toast;
-
-public class SystemBroadcastReceiver extends BroadcastReceiver {
-  @Override
-  public void onReceive(Context context, Intent intent) {
-     Toast.makeText(context, "Intent Detected.", Toast.LENGTH_LONG).show();
-  }
-
-}
+//package com.appgemacht.common;
+//
+//import android.content.BroadcastReceiver;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.widget.Toast;
+//
+//public class SystemBroadcastReceiver extends BroadcastReceiver {
+//  @Override
+//  public void onReceive(Context context, Intent intent) {
+//     Toast.makeText(context, "Intent Detected.", Toast.LENGTH_LONG).show();
+//  }
+//
+//}
